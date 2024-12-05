f690bdb78b8a577e2cdaf9579557d742ff0aac6b
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2024-12-05 22:25:18 +0330

    complect second page of onboarding

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Quality from "../components/Onboarding/quality";
+import SatisFaction from "../components/Onboarding/satisfaction"
 
 
 const Onboarding = () => {
@@ -8,6 +9,7 @@ const Onboarding = () => {
   return (
     <div className="relative w-full min-h-screen flex flex-col justify-center items-center">
       {page === 0 && <Quality setPage={setPage} />}
+      {page === 1 && <SatisFaction setPage={setPage} />}
 
     </div>
   );
