97a27aac53cfc2246aeef6ccebb4943e44489552
asal-aslani <asal.aslanii1383@gmail.com>
2024-12-05 15:50:38 +0330

    created page loading

---
+++ src/components/siteLogo.tsx
@@ -0,0 +1,7 @@
+export default function SiteLogo (){
+    return(
+      <div className="logoContainer flex justify-center items-center gap-[12px] rounded-full bg-black w-8 h-8 ">
+    <img className="w-6 h-6 m-auto " src="/src/assets/logo.svg" alt="" />
+      </div>
+    )    
+    }
--- src/pages/pageLoading.tsx
+++ src/pages/pageLoading.tsx
@@ -1,8 +1,14 @@
 import Loading from "../components/loadingLogo";
+import SiteTitle from "../components/siteTitle";
+import SiteLogo from "../components/siteLogo";
 
 export default function LoadingPage(){
     return(
-    <div>
+    <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-row justify-center items-center mt-72"><SiteLogo/>
+        <SiteTitle/>
+        </div>
+        
         <Loading/>
     </div>
 )
