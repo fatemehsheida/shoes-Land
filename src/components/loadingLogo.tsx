3eae411d6fd751adbff384f456b96e918caade54
asal-aslani <asal.aslanii1383@gmail.com>
2024-12-05 15:21:25 +0330

    created loading logo

---
+++ src/components/loadingLogo.tsx
@@ -0,0 +1,7 @@
+export default function Loading (){
+return(
+    <div className="loading-ring w-[50px] h-[50px] border-8 border-slate-800 border-solid border-t-[6px] border-t-white rounded-full animate-spin fixed top-[80%]">
+        
+    </div>
+)    
+}
---
+++ src/pages/pageLoading.tsx
@@ -0,0 +1,9 @@
+import Loading from "../components/loadingLogo";
+
+export default function LoadingPage(){
+    return(
+    <div>
+        <Loading/>
+    </div>
+)
+}
