bc61623c035da80863ad562ff5c8797ad2de372c
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2024-12-06 15:20:00 +0330

    create login

---
+++ src/components/Onboarding/shoearight.tsx
@@ -0,0 +1,32 @@
+interface ShoeaRight {
+  setPage: (value: number) => void;
+}
+const Page3: React.FC<ShoeaRight> = ({ setPage }) => {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-centerp">
+      <div className="w-full h-[60%]">
+        <img src="/src/assets/obsp3-img.png" className="w-full h-full" />
+      </div>
+      <div className="relative w-full h-full flex flex-col items-center gap-16 pt-8 px-6">
+        <p className="text-2xl leading-relaxed font-bold text-center">
+          Let’s fulfill your fashion needs with shoearight now!
+        </p>
+        <div className="felx flex-col">
+          <div className=" flex gap-2 absolute bottom-32 left-[40%]">
+            <div className="w-8 h-1 bg-black opacity-50"></div>
+            <div className="w-8 h-1 bg-black opacity-50"></div>
+            <div className="w-8 h-1 bg-black"></div>
+          </div>
+          <button
+            onClick={() => setPage(0)}
+            className="bg-black hover:bg-gray-800 rounded-full text-white w-[90%] py-3 fixed bottom-8 left-[5%]"
+          >
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page3;
---
+++ src/pages/Login/login.tsx
@@ -0,0 +1,9 @@
+import FormLogin from "../../components/Login/FormLogin";
+
+export default function Login() {
+  return (
+    <div>
+      <FormLogin />
+    </div>
+  );
+}
