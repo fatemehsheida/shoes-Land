7244c893dd66a0994a796d97897bf06ba2e7ffff
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2024-12-19 00:54:04 +0330

    Refactor all onboarding pages

--- src/components/loadingLogo.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading (){
-return(
-    <div className="loading-ring w-[50px] h-[50px] border-8 border-slate-800 border-solid border-t-[6px] border-t-white rounded-full animate-spin fixed top-[80%]">
-        
-    </div>
-)    
-}
--- src/components/siteTitle.tsx
+++
@@ -1,7 +0,0 @@
-export default function SiteTitle (){
-    return(
-      <div>
-        <h1 className="font-bold text-5xl text-slate-800">Shoea</h1>
-      </div>
-    )    
-    }
