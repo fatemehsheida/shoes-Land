2f543a3fbe2bb0891367fc1af5a9f4a91ee5d163
𝓕𝓽𝓶 <167397935+fatemehsheida@users.noreply.github.com>
2025-01-14 17:38:19 +0330

    sign in page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -77,7 +77,7 @@ export default function SingIN() {
       </div>
       <div>
         <form onSubmit={handleSubmit((data) => console.log(data))}>
-            {/* usernamefiled */}
+          {/* usernamefiled */}
           <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -93,11 +93,16 @@ export default function SingIN() {
                 d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
               />
             </svg>
-            <input placeholder="Username" {...register }/>
+            <input
+              placeholder="Username"
+              {...register("username", { required: true, maxLength: 20 })}
+            />
           </div>
           {/* emailfield */}
+          <div></div>
+          <input />
           {/* firstname */}
-          <div> 
+          <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -115,7 +120,7 @@ export default function SingIN() {
             <input placeholder="Username" />
           </div>
           {/* lastname */}
-          <div> 
+          <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
